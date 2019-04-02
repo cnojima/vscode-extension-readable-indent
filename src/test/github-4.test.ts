572e09c3cdfb77bf90054edcd8bb76eeebe82a38
Chris Nojima <cnojima@gmail.com>
2019-04-02 16:19:40 -0700

    test: Adding tests and support files for github issue #4.

--- src/test/github-4.test.ts
+++ src/test/github-4.test.ts
@@ -13,12 +13,23 @@ const supportPath = path.resolve(__dirname, '../../src/test/support');
 suite("Github Issue #4", function () {
   const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('readableIndent');
 
-  test('Github Issue 4 test', () => {
+  test('Github Issue 4 test :: left-justified', () => {
     const code = fs.readFileSync(path.resolve(supportPath, 'github-4-raw.txt'), 'utf-8');
     const expected = fs.readFileSync(path.resolve(supportPath, 'github-4-expected.txt'), 'utf-8');
     let foo;
 
     const ind = new Indenter(code, { alphabetize: false });
+    foo = ind.indent();
+    assert.equal(foo, expected);
+  });
+
+  test('Github Issue 4.1 test :: center-justified', () => {
+    const code = fs.readFileSync(path.resolve(supportPath, 'github-4.1-raw.txt'), 'utf-8');
+    const expected = fs.readFileSync(path.resolve(supportPath, 'github-4.1-expected.txt'), 'utf-8');
+    let foo;
+
+    const ind = new Indenter(code, { alphabetize: false });
+    ind.centerJustify = true;
     foo = ind.indent();
     assert.equal(foo, expected);
   });
