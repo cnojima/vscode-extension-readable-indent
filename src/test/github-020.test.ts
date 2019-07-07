33239111483396ccdcc1b0583b425e89a4fbd5a7
Chris Nojima <cnojima@gmail.com>
2019-07-07 11:25:36 -0700

    test(fat-arrow): Test fat-arrow functionality with all supported functionality.

--- src/test/github-020.test.ts
+++ src/test/github-020.test.ts
@@ -10,22 +10,44 @@ import hash from '../util/hash';
 
 const supportPath = path.resolve(__dirname, '../../src/test/support');
 const raw         = fs.readFileSync(path.resolve(supportPath, 'github-020-raw.txt'), 'utf-8');
-const expected    = fs.readFileSync(path.resolve(supportPath, 'github-020-expected.txt'), 'utf-8');
-const raw1         = fs.readFileSync(path.resolve(supportPath, 'github-020.1-raw.txt'), 'utf-8');
 const expected1    = fs.readFileSync(path.resolve(supportPath, 'github-020.1-expected.txt'), 'utf-8');
+const expected2    = fs.readFileSync(path.resolve(supportPath, 'github-020.2-expected.txt'), 'utf-8');
+const expected3    = fs.readFileSync(path.resolve(supportPath, 'github-020.3-expected.txt'), 'utf-8');
+const expected4    = fs.readFileSync(path.resolve(supportPath, 'github-020.4-expected.txt'), 'utf-8');
 
 suite('Github Issue #20 Tests', () => {
   test('RI should handle fat-arrow as a pivot char sequence', () => {
     const ind  = new Indenter();
     const res = ind.indent(raw);
 
-    assert.equal(res, expected);
+    assert.equal(res, expected1);
   });
 
-  test('RI should handle fat-arrow as a pivot char sequence with exisiting indentation', () => {
+  test('RI should handle fat-arrow as a pivot char sequence, alphabetical', () => {
     const ind  = new Indenter();
-    const res = ind.indent(raw1);
+    ind.alphabetize = true;
 
-    assert.equal(res, expected1);
+    const res = ind.indent(raw);
+
+    assert.equal(res, expected2);
+  });
+
+  test('RI should handle fat-arrow as a pivot char sequence, centered', () => {
+    const ind  = new Indenter();
+    ind.centerJustify = true;
+
+    const res = ind.indent(raw);
+
+    assert.equal(res, expected3);
+  });
+
+  test('RI should handle fat-arrow as a pivot char sequence, centered alphabetically', () => {
+    const ind  = new Indenter();
+    ind.alphabetize = true;
+    ind.centerJustify = true;
+
+    const res = ind.indent(raw);
+
+    assert.equal(res, expected4);
   });
 });
