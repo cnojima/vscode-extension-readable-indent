f333b4960655c2d1aadde1298f21238ed6d3b10b
Chris Nojima <cnojima@gmail.com>
2019-07-07 11:14:33 -0700

    test(fat-arrow): Add tests for fat-arrow handling.

--- src/test/github-020.test.ts
+++ src/test/github-020.test.ts
@@ -9,14 +9,23 @@ import Indenter from '../Indenter';
 import hash from '../util/hash';
 
 const supportPath = path.resolve(__dirname, '../../src/test/support');
-const raw         = fs.readFileSync(path.resolve(supportPath, 'github-20-raw.txt'), 'utf-8');
-const expected    = fs.readFileSync(path.resolve(supportPath, 'github-20-expected.txt'), 'utf-8');
+const raw         = fs.readFileSync(path.resolve(supportPath, 'github-020-raw.txt'), 'utf-8');
+const expected    = fs.readFileSync(path.resolve(supportPath, 'github-020-expected.txt'), 'utf-8');
+const raw1         = fs.readFileSync(path.resolve(supportPath, 'github-020.1-raw.txt'), 'utf-8');
+const expected1    = fs.readFileSync(path.resolve(supportPath, 'github-020.1-expected.txt'), 'utf-8');
 
-suite('Github Issue #12 Tests', () => {
-  test('subsequent RI functions should use reset value, pivots, etc.', () => {
+suite('Github Issue #20 Tests', () => {
+  test('RI should handle fat-arrow as a pivot char sequence', () => {
     const ind  = new Indenter();
     const res = ind.indent(raw);
 
     assert.equal(res, expected);
+  });
+
+  test('RI should handle fat-arrow as a pivot char sequence with exisiting indentation', () => {
+    const ind  = new Indenter();
+    const res = ind.indent(raw1);
+
+    assert.equal(res, expected1);
   });
 });
